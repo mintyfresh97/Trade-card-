161c2c5f1a08af3bc344fcecb2341d754fc77c56
mintyfresh97 <94802870+mintyfresh97@users.noreply.github.com>
2025-03-31 17:24:25 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="PnL & Risk Dashboard", layout="wide")
+st.markdown("<h1 style='color:white;'>PnL & Risk Dashboard</h1>", unsafe_allow_html=True)
+
+my_component = components.declare_component(
+    name="my_component",
+    path="frontend/my_component/build"
+)
+
+response = my_component()
+
+if response:
+    st.write("Component says:")
+    st.json(response)
